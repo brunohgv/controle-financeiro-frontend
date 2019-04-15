6f1189f8ce40dc2803edfe167845f66faf036fc3
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-04-14 23:32:37 -0300

    Finalização da tela de listagem

--- src/pages/transactions/transactions.js
+++ src/pages/transactions/transactions.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Table, TableHead, TableCell, TableBody, TableRow, Button, Dialog, DialogTitle, DialogContent, DialogActions, Input } from '@material-ui/core'
+import { MdDelete } from 'react-icons/md'
+
 import api from '../../services/api';
 
 export default class Transactions extends Component {
@@ -12,29 +14,46 @@ export default class Transactions extends Component {
 
   state = {
       transactions: [],
-      dialogOpen: false,
+      newTransactionDialogOpen: false,
+      deleteTransactionDialogOpen: false,
       newTransaction: {
           description: '',
           value: '',
           date: new Date()
-      }
+      },
+      selectedTransaction: {}
   }
   
-  handleButtonClick = () => {
+  handleOpenNewTransactionDialog = () => {
     this.setState({
-        dialogOpen: true
+      newTransactionDialogOpen: true
     })
   }
 
-  handleCancel = () => {
+  handleOpenDeleteTransactionDialog = (transaction) => {
+      let selectedTransaction = transaction
+      this.setState({
+        deleteTransactionDialogOpen: true,
+        selectedTransaction: selectedTransaction
+      })
+  }
+
+  handleCancelCreate = () => {
     const emptyTransaction = this.state.newTransaction
     emptyTransaction.description = ''
     emptyTransaction.value = 0
     emptyTransaction.date = new Date()
     this.setState({
       newTransaction: emptyTransaction,
-      dialogOpen: false
+      newTransactionDialogOpen: false
     })
+  }
+
+  handleCancelDelete = () => {
+      this.setState({
+          deleteTransactionDialogOpen: false,
+          selectedTransaction: {}
+      })
   }
 
   handleSave = async () => {
@@ -48,7 +67,7 @@ export default class Transactions extends Component {
     this.setState({
       transactions: updatedList
     })
-    this.handleCancel()
+    this.handleCancelCreate()
   }
 
   handleChange = (event) => {
@@ -61,14 +80,30 @@ export default class Transactions extends Component {
     })
   }
 
+  handleDelete = async (transactionId) => {
+    const deleteResponse = await api.delete(`/transactions/${transactionId}`)
+    let updatedTransactions = this.state.transactions
+    if (deleteResponse.data.n === 1) {
+        updatedTransactions = updatedTransactions.filter(element => {
+            return element._id !== transactionId
+        })
+        this.setState({
+            transactions: updatedTransactions
+        })
+    } else {
+        console.error('Algum erro aconteceu durante a deleção')
+    }
+    this.handleCancelDelete()
+  }
+
   render() {
     return (
         <div>
-            <Button variant="contained" onClick={this.handleButtonClick} color="primary">
+            <Button variant="contained" onClick={this.handleOpenNewTransactionDialog} color="primary">
                 Adicionar Transação
             </Button>
 
-            <Dialog open={this.state.dialogOpen}
+            <Dialog open={this.state.newTransactionDialogOpen}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description">
                 <DialogTitle id="alert-dialog-title">Adicionar Transação</DialogTitle>
@@ -81,36 +116,80 @@ export default class Transactions extends Component {
                     </div>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={this.handleCancel}>
+                  <Button onClick={this.handleCancelCreate}>
                     Cancelar
-                    </Button>
-                    <Button onClick={this.handleSave}>
+                  </Button>
+                  <Button style={style.successButton} onClick={this.handleSave}>
                     Salvar
-                    </Button>
+                  </Button>
                 </DialogActions>
             </Dialog>
 
             <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Id</TableCell>
-                        <TableCell>Descrição</TableCell>
-                        <TableCell>Valor</TableCell>
-                        <TableCell>Data</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    { this.state.transactions && this.state.transactions.map(transaction => (
-                        <TableRow key={transaction._id}>
-                            <TableCell>{transaction._id}</TableCell>
-                            <TableCell>{transaction.description}</TableCell>
-                            <TableCell>{transaction.value}</TableCell>
-                            <TableCell>{ transaction.date }</TableCell>
-                        </TableRow>
-                    )) }
-                </TableBody>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell>Descrição</TableCell>
+                  <TableCell>Valor (R$)</TableCell>
+                  <TableCell>Data</TableCell>
+                  <TableCell>Ações</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                { this.state.transactions && this.state.transactions.map(transaction => (
+                  <TableRow key={transaction._id}>
+                    <TableCell>{transaction._id}</TableCell>
+                    <TableCell>{transaction.description}</TableCell>
+                    <TableCell style={transaction.value > 0 ? style.positiveTransaction : style.negativeTransaction}>{transaction.value}</TableCell>
+                    <TableCell>{ new Date(transaction.date).toLocaleDateString('pt-BR') }</TableCell>
+                    <TableCell>
+                      <MdDelete
+                          size={18}
+                          style={style.deleteIcon}
+                          onClick={this.handleOpenDeleteTransactionDialog.bind(this, transaction)} />
+                    </TableCell>
+                  </TableRow>
+                )) }
+              </TableBody>
             </Table>
+
+            <Dialog open={this.state.deleteTransactionDialogOpen}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title">Deletar Transação</DialogTitle>
+                <DialogContent>
+                  Tem certeza que deseja deletar a seguinte transação? <br /><br />
+                  <strong>Descrição:</strong> { this.state.selectedTransaction.description } <br />
+                  <strong>Valor:</strong> R$ { this.state.selectedTransaction.value } <br />
+                  <strong>Data:</strong> { new Date(this.state.selectedTransaction.date).toLocaleDateString('pt-BR') }
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleCancelDelete}>Cancelar</Button>
+                    <Button style={style.deleteButton} onClick={this.handleDelete.bind(this, this.state.selectedTransaction._id)}>Deletar</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
   }
+}
+
+const style = {
+    deleteIcon: {
+        color: '#C82433',
+        cursor: 'pointer'
+    },
+    positiveTransaction: {
+        color: '#218839'
+    },
+    negativeTransaction: {
+        color: '#C82433'
+    },
+    successButton: {
+      backgroundColor: '#218839',
+      color: '#fff'
+    },
+    deleteButton: {
+      backgroundColor: '#C82433',
+      color: '#fff'
+    }
 }
